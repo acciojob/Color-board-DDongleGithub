4bb0a97634cb1cff58fd97a8e50ba0512a96ab0b
DDongleGithub <120495126+DDongleGithub@users.noreply.github.com>
2023-07-26 14:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const board = document.getElementById("board");
+
+  // Create 800 boxes and append them to the board
+  for (let i = 0; i < 800; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    board.appendChild(box);
+  }
+});
